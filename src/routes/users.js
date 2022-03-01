b82d78760612a1db4ea0dd5322e7b8511681fe8c
sikeda <sikeda@FMX-P20210403.local>
2022-03-01 15:53:46 +0900

    mongo接続追加

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,42 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const { Sequelize } = require('sequelize')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const sequelize = new Sequelize('test', 'user', 'password', {
   host: 'mysql',
   dialect: 'mysql',
 })
 
-// const initTestSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number
-// })
-
-// const options = {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   user: 'root',
-//   pass: 'password',
-//   dbName: 'test'
-// }
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
 
 router.get('/', async (req, res, next) => {
   try {
+    console.log('start connect to Mysql')
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
-    // await mongoose
-    //   .connect('mongodb://127.0.0.1:27017/', options)
-    //   .then(() => {
-    //     console.log('successfully connected to the database')
-    //   })
-    //   .catch((err) => {
-    //     console.log('error connecting to the database')
-    //     console.log(err)
-    //     process.exit()
-    //   })
   } catch (error) {
     console.error('Unable to connect to the database:', error)
+  }
+
+  try {
+    console.log('start connect to Mongo')
+        // await mongoose.connect('mongodb://localhost:27017/test', options)
+        await mongoose.connect('mongodb://root:example@mongo:27017/', {useNewUrlParser: true})
+        // const db = mongoose.connection
+        // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+        // db.once('open', () => console.log('MongoDB connection successful'))
+        await mongoose.connection.close()
+        console.log('success')
+  } catch (err) {
+    console.log('Unable to connect Mongo:', err)
   }
   res.send('respond with a resource')
 })
